52ae66e3f1117b1c35cae168643ff06d630bfbac
EmilioAlarconGomez <emilio.alarcon.gomez@gmail.com>
2025-04-16 17:56:29 -0400

    add P2

---
+++ Operadores Aritmeticos.js
@@ -0,0 +1,14 @@
+const numero1 = 20;
+const numero2 = 40;
+
+const suma = numero1 + numero2;
+const resta = numero1 - numero2;
+const multiplicacion = numero1 * numero2;
+const division = numero1 / numero2;
+const modulo = numero1 % numero2;
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
+console.log("Módulo: " + modulo);

bcb5f9ddd5116661356fa884d0d548da0ee8cc77
EmilioAlarconGomez <emilio.alarcon.gomez@gmail.com>
2025-04-16 19:21:42 -0400

    add P8

---
+++ Objeto.js
@@ -0,0 +1,26 @@
+const persona = {
+    nombre: "Emilio",
+    edad: 22,
+    genero: "masculino",
+}
+console.log(persona)
+
+const caja = {
+    cuadernos: 3,
+    lapices: 14,
+    papel: 2,
+    fotografias: 0,
+    estado: "bueno"
+}
+console.log(caja)
+console.log(caja.cuadernos)
+console.log(caja.lapices)
+console.log(caja.papel)
+console.log(caja.fotografias)
+console.log(caja.estado)
+console.log(typeof caja)
+console.log(typeof caja.cuadernos)
+console.log(typeof caja.lapices)
+console.log(typeof caja.papel)
+console.log(typeof caja.fotografias)
+console.log(typeof caja.estado)
